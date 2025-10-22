650caaaaf4db790074b338db3ff9fdecee85a8b5
Max <Max@DESKTOP-B8E2B53>
2025-10-22 14:48:45 +0200

    init: Create a UI for measurement overview
    Adds the main window UI for visualizing collected sensor data.
    The interface provides:
    - A selector bar for filtering data by station ID, sensor ID, and time span.
    - An area for plotting the retrieved measurements (using ScottPlot).
    - A status bar to indicate data update status.

---
+++ WPFClient/Controller/DelegateCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFClient.DelegateCommands
+{
+    /// <summary>
+    /// Implementierung des ICommand-Interfaces, die es ermöglicht,
+    /// Commands mit Delegates für Execute und CanExecute zu verwenden.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object>? canExecute;
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der <see cref="DelegateCommand"/>-Klasse mit CanExecute und Execute-Delegates.
+        /// </summary>
+        /// <param name="canExecute">Prüft, ob der Command ausgeführt werden kann.</param>
+        /// <param name="execute">Auszuführende Aktion.</param>
+        public DelegateCommand(Predicate<object>? canExecute, Action<object> execute) =>
+            (this.canExecute, this.execute) = (canExecute, execute);
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der <see cref="DelegateCommand"/>-Klasse mit nur einem Execute-Delegate.
+        /// Der Command ist immer ausführbar.
+        /// </summary>
+        /// <param name="execute">Auszuführende Aktion.</param>
+        public DelegateCommand(Action<object> execute) : this(null, execute) { }
+
+        /// <summary>
+        /// Wird ausgelöst, wenn sich der Ausführbarkeitsstatus des Commands ändert.
+        /// </summary>
+        public event EventHandler? CanExecuteChanged;
+
+        /// <summary>
+        /// Löst das <see cref="CanExecuteChanged"/>-Event aus, um die UI über Änderungen zu informieren.
+        /// </summary>
+        public void RaiseCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        /// <summary>
+        /// Prüft, ob der Command aktuell ausgeführt werden kann.
+        /// </summary>
+        /// <param name="parameter">Parameter für die Prüfung.</param>
+        /// <returns>True, wenn ausführbar; sonst false.</returns>
+        public bool CanExecute(object? parameter) => this.canExecute?.Invoke(parameter) ?? true;
+
+        /// <summary>
+        /// Führt die hinterlegte Aktion aus.
+        /// </summary>
+        /// <param name="parameter">Parameter für die Ausführung.</param>
+        public void Execute(object? parameter)
+        {
+            this.execute?.Invoke(parameter);
+        }
+    }
+}
---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.ComponentModel;
+using WPFClient.ViewModels;
+using System.Threading.Tasks;
+
+namespace WPFClient
+{
+    public partial class MainWindow : Window
+    {
+        // Speichern Sie das ViewModel, um später im Closing-Event darauf zugreifen zu können
+        private MainWindowViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new MainWindowViewModel();
+
+            _viewModel.MeasurementsPlot = wpfPlot.Plot;
+
+            this.DataContext = _viewModel;
+
+            _viewModel.RefreshRequired += ViewModel_RefreshRequired;
+
+            _ = _viewModel.InitialLoadDataAsync(plotAfterwards: true);
+        }
+
+        private void ViewModel_RefreshRequired()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                wpfPlot.Refresh();
+            });
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (_viewModel != null)
+            {
+                _viewModel.RefreshRequired -= ViewModel_RefreshRequired;
+            }
+
+            MessageBoxResult messageBoxResult = MessageBox.Show(
+                "Sie schließen gerade die Pflanzenübersicht!\n" +
+                "Wollen Sie dies wirklich tun?", "PlantStationView schließen?", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+
+            if (messageBoxResult == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
