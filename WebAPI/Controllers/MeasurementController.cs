78cf829212a564b78b4cd1c8230ab3e2e9588711
Max <Max@DESKTOP-B8E2B53>
2025-10-27 10:43:49 +0100

    feat: Added documentaion strings for all API-Controller.

--- WebAPI/Controllers/MeasurementController.cs
+++ WebAPI/Controllers/MeasurementController.cs
@@ -1,60 +1,144 @@
 ﻿using DataAccess;
+using DataAccess.Interfaces;
 using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace PlantStationAPI.Backend.Controllers;
-[ApiController]
-[Route("api/[controller]/[action]")]
-public class MeasurementController : ControllerBase
+namespace PlantStationAPI.Backend.Controllers
 {
-    private readonly ApiContext _context;
-
-    public MeasurementController(ApiContext context)
+    /// <summary>
+    /// API Controller for handling CRUD operations and retrieval of <see cref="Measurement"/> data.
+    /// It provides endpoints for creating new measurements and fetching existing measurements based on ID, sensor ID, and time criteria.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MeasurementController(IMeasurementRepo measurementRepo, ISensorRepo sensorRepo, IStationRepo stationRepo) : ControllerBase
     {
-        _context = context;
-    }
+        private readonly ISensorRepo _sensorRepo = sensorRepo;
+        private readonly IMeasurementRepo _measurementRepo = measurementRepo;
+        private readonly IStationRepo _stationRepo = stationRepo;
 
-    [HttpPost]
-    public IActionResult Create(Measurement measurement)
-    {
-        if (measurement == null)
+        /// <summary>
+        /// Creates a new measurement entry in the database.
+        /// </summary>
+        /// <param name="measurement">The <see cref="Measurement"/> object to be created.</param>
+        /// <returns>
+        /// An <see cref="IActionResult"/>:
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) if creation is successful.</description></item>
+        /// <item><description><see cref="ControllerBase.BadRequest(object)"/> (400) if the input measurement is null, has a non-zero ID, or the associated sensor ID is not found.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpPost]
+        public async Task<IActionResult> Create(Measurement measurement)
         {
-            return BadRequest("measurment can not be null!");
+            if (measurement == null)
+            {
+                return BadRequest("measurment can not be null!");
+            }
+
+            if (measurement.Id != 0)
+            {
+                return BadRequest("The id of an new entry has to be 0!");
+            }
+
+            var sensor = await _measurementRepo.Create(measurement);
+
+            if (sensor == null)
+            {
+                return BadRequest($"Measurement can not be stored. No sensor with the ID: {measurement.SensorId} found.");
+            }
+
+            return Ok(measurement);
         }
 
-        if (measurement.Id != 0)
+        /// <summary>
+        /// Retrieves a single measurement by its unique identifier (ID).
+        /// </summary>
+        /// <param name="id">The unique integer ID of the measurement.</param>
+        /// <returns>
+        /// An <see cref="IActionResult"/>:
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) with the <see cref="Measurement"/> object if found.</description></item>
+        /// <item><description><see cref="ControllerBase.BadRequest(object)"/> (400) if the ID is not greater than zero.</description></item>
+        /// <item><description><see cref="ControllerBase.NotFound"/> (404) if no measurement with the given ID exists.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
         {
-            return BadRequest("The id of an new entry has to be 0!");
+            if (id <= 0)
+            {
+                return BadRequest("id must be greater than 0");
+            }
+
+            var result = await _measurementRepo.GetById(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
         }
 
-        var sensor = _context.Sensors.Find(measurement.SensorId);
-        if (sensor == null)
+        /// <summary>
+        /// Retrieves the latest measurements for a specific sensor.
+        /// </summary>
+        /// <param name="sensorId">The ID of the sensor whose measurements are to be retrieved.</param>
+        /// <param name="count">The maximum number of measurements to return. Defaults to 180.</param>
+        /// <returns>
+        /// An <see cref="IActionResult"/>:
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) with the list of <see cref="Measurement"/> objects.</description></item>
+        /// <item><description><see cref="ControllerBase.NotFound"/> (404) if no measurements are found for the given sensor ID.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpGet]
+        public async Task<IActionResult> GetLastOfSensor(int sensorId, int count = 180)
         {
-            return BadRequest($"Measurement can not be stored. No sensor with the ID: {measurement.SensorId} found.");
-        }
-        //
-        // var station = _context.Stations.Find(measurement.Sensor.StationId);
-        // if (station == null)
-        // {
-        //     return new JsonResult(BadRequest(
-        //         $"Measurement can not be stored. No sensor with the ID: {measurement.Sensor.StationId} found."));
-        // }
-        _context.Measurements.Add(measurement);
-        _context.SaveChanges();
+            List<Measurement> result = await _measurementRepo.GetLastOfSensor(sensorId, count);
 
-        return Ok(measurement);
-    }
+            if (result.IsNullOrEmpty())
+            {
+                return NotFound();
+            }
 
-    [HttpGet]
-    public IActionResult Get(int id)
-    {
-        var result = _context.Measurements.Find(id);
-
-        if (result == null)
-        {
-            return NotFound();
+            return Ok(result);
         }
 
-        return Ok(result);
+        /// <summary>
+        /// Retrieves all measurements for a specific sensor recorded since a given date and time.
+        /// </summary>
+        /// <param name="sensorId">The ID of the sensor whose measurements are to be retrieved.</param>
+        /// <param name="since">The minimum date and time from which measurements should be included.</param>
+        /// <returns>
+        /// An <see cref="IActionResult"/>:
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) with the list of <see cref="Measurement"/> objects.</description></item>
+        /// <item><description><see cref="ControllerBase.BadRequest(object)"/> (400) if the 'since' parameter is not a valid date.</description></item>
+        /// <item><description><see cref="ControllerBase.NotFound"/> (404) if no measurements are found within the specified time frame.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpGet]
+        public async Task<IActionResult> GetLastOfSensorSince(int sensorId, DateTime since)
+        {
+            if (since <= DateTime.MinValue)
+            {
+                return BadRequest("since must be a valid date");
+            }
+
+            var result = await _measurementRepo.GetLastOfSensorSince(sensorId, since);
+
+            if (result.IsNullOrEmpty())
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
     }
 }
--- WebAPI/Controllers/StationController.cs
+++ WebAPI/Controllers/StationController.cs
@@ -1,66 +1,144 @@
 ﻿using DataAccess;
+using DataAccess.Interfaces;
 using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System; // Required for Exception
 
-namespace PlantStationAPI.Backend.Controllers;
-[ApiController]
-[Route("api/[controller]/[action]")]
-public class StationController : ControllerBase
+namespace PlantStationAPI.Backend.Controllers
 {
-    private readonly ApiContext _context;
-
-    public StationController(ApiContext context)
+    /// <summary>
+    /// API Controller responsible for handling CRUD operations and retrieval of <see cref="Station"/> entities.
+    /// It provides endpoints to create new stations, and to fetch stations or station IDs based on various criteria.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class StationController(IMeasurementRepo measurementRepo, ISensorRepo sensorRepo, IStationRepo stationRepo) : ControllerBase
     {
-        _context = context;
-    }
+        private readonly ISensorRepo _sensorRepo = sensorRepo; 
+        private readonly IMeasurementRepo _measurementRepo = measurementRepo; 
+        private readonly IStationRepo _stationRepo = stationRepo; 
 
-    [HttpPost]
-    public JsonResult Create(Station station)
-    {
-        if (station.Id != 0)
+        /// <summary>
+        /// Creates a new station entry in the database.
+        /// </summary>
+        /// <param name="station">The <see cref="Station"/> object to be created.</param>
+        /// <returns>
+        /// A <see cref="JsonResult"/> containing an <see cref="IActionResult"/>:
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) with the created <see cref="Station"/> object if successful.</description></item>
+        /// <item><description><see cref="ControllerBase.BadRequest(object)"/> (400) if the station's ID is not zero, or if the repository fails to create the station.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpPost]
+        public async Task<JsonResult> Create(Station station)
         {
-            return new JsonResult(BadRequest("The id of an new entry has to be 0!"));
+            if (station.Id != 0)
+            {
+                return new JsonResult(BadRequest("The id of an new entry has to be 0!"));
+            }
+
+            Station stationFromDb = await _stationRepo.Create(station);
+
+            if (stationFromDb == null)
+            {
+                return new JsonResult(BadRequest("Station could not be created!"));
+            }
+
+            return new JsonResult(Ok(stationFromDb));
         }
 
-        _context.Stations.Add(station);
-        _context.SaveChanges();
+        /// <summary>
+        /// Retrieves a single station by its unique identifier (ID).
+        /// </summary>
+        /// <param name="stationId">The unique integer ID of the station.</param>
+        /// <returns>
+        /// A <see cref="JsonResult"/> containing an <see cref="IActionResult"/>:
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) with the <see cref="Station"/> object if found.</description></item>
+        /// <item><description><see cref="ControllerBase.BadRequest(object)"/> (400) if the ID is zero, no station is found, or an exception occurs during lookup.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpGet("{stationId}")]
+        public async Task<JsonResult> GetStation(int stationId)
+        {
+            if (stationId == 0)
+            {
+                return new JsonResult(BadRequest("The id of a station has to be greater than 0!"));
+            }
+            Station? stationFromDb;
+            try
+            {
+                stationFromDb = await _stationRepo.GetById(stationId);
+            }
+            catch (Exception)
+            {
+                return new JsonResult(BadRequest($"No matching station by StationId: {stationId}"));
+            }
 
-        return new JsonResult(Ok(station));
-    }
+            if (stationFromDb == null)
+            {
+                return new JsonResult(BadRequest($"No matching station by StationId: {stationId}!"));
+            }
 
-    [HttpGet("{stationId}")]
-    public JsonResult GetStation(int stationId) 
-    {
-        if (stationId == 0)
-        {
-            return new JsonResult(BadRequest("The id of a station has to be greater than 0!"));
-        }
-        Station station;
-        try
-        {
-            station = _context.Stations.Find(stationId);
-        }
-        catch (Exception)
-        {
-            return new JsonResult(BadRequest($"No matching station by StationId: {stationId}"));
-        }
-
-        return new JsonResult(Ok(station)); 
-    }
-
-    [HttpGet]
-    public ActionResult GetAllStations() 
-    {
-        List<Station> stations;
-        try
-        {
-            stations = _context.Stations.ToList();
-        }
-        catch (Exception)
-        {
-            return new JsonResult(BadRequest($"No station found!"));
+            return new JsonResult(Ok(stationFromDb));
         }
 
-        return Ok(stations); 
+        /// <summary>
+        /// Retrieves a list of all station entities stored in the database.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="ActionResult"/> (which can contain a <see cref="JsonResult"/> or <see cref="OkObjectResult"/>):
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) with the list of <see cref="Station"/> objects if successful.</description></item>
+        /// <item><description><see cref="ControllerBase.BadRequest(object)"/> (400) if no stations are found or an exception occurs during retrieval.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpGet]
+        public async Task<ActionResult> GetAllStations()
+        {
+            List<Station> stations;
+            try
+            {
+                stations = await _stationRepo.GetAll();
+            }
+            catch (Exception)
+            {
+                return new JsonResult(BadRequest($"No station found!"));
+            }
+
+            if (stations == null)
+            {
+                return new JsonResult(BadRequest($"No station found!"));
+            }
+
+            return Ok(stations);
+        }
+
+        /// <summary>
+        /// Retrieves a list of all unique station IDs currently stored in the database.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="ActionResult"/> (which can contain a <see cref="JsonResult"/> or <see cref="OkObjectResult"/>):
+        /// <list type="bullet">
+        /// <item><description><see cref="ControllerBase.Ok(object)"/> (200) with the list of integer station IDs if successful.</description></item>
+        /// <item><description><see cref="ControllerBase.BadRequest(object)"/> (400) if no station IDs are found or an exception occurs during retrieval.</description></item>
+        /// </list>
+        /// </returns>
+        [HttpGet]
+        public async Task<ActionResult> GetIds()
+        {
+            List<int> stationIds;
+            try
+            {
+                stationIds = await _stationRepo.GetAllIds();
+            }
+            catch (Exception)
+            {
+                return new JsonResult(BadRequest($"No station found!"));
+            }
+            return Ok(stationIds);
+        }
     }
 }
