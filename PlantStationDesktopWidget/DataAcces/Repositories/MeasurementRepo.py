6bc9f38edbed06f11068b3dab0d9f5596409f7c4
max <maxwoehlke@web.de>
2026-04-04 23:04:25 +0200

    refact: changes the SQL queries from mapping to DTO's in mapping to Dataframes
    feat: implements a validation of the measurement values by measurement bounds.
    - temperature -40 to 60 °C
    - humidity and soil moisture 0 to 100 %(rel)

--- PlantStationDesktopWidget/DataAcces/Repositories/MeasurementRepo.py
+++ PlantStationDesktopWidget/DataAcces/Repositories/MeasurementRepo.py
@@ -1,12 +1,6 @@
-from typing import List
-
-from DataAcces.DTOs.StationDTO import StationDTO
-from DataAcces.Models.Base import SessionLocal
 from DataAcces.Models.Measurement import Measurement
 from DataAcces.DTOs.MeasurementDTO import MeasurementDTO
-from DataAcces.DTOs.SensorDTO import SensorDTO
-
-from datetime import datetime, timedelta, timezone
+import pandas as pd
 
 
 class MeasurementRepo:
@@ -14,86 +8,83 @@ class MeasurementRepo:
     def __init__(self, session_factory=SessionLocal):
         self.session_factory = session_factory
 
-    # Alle Messungen abrufen
-    def get_all(self) -> List[MeasurementDTO]:
+    def get_all(self) -> pd.DataFrame:
         with self.session_factory() as session:
-            measurements = session.query(Measurement).all()
-            return [self._map_to_dto(m) for m in measurements]
+            query = session.query(Measurement)
+            df = pd.read_sql(query.statement, session.bind)
 
-    # Einzelne Messung nach ID
-    def get_by_id(self, measurement_id: int) -> MeasurementDTO | None:
+            if df.empty:
+                return df
+
+            df["RecordedAt"] = self.convert_time_to_local(df["RecordedAt"])
+
+            return df[df.apply(self.is_measurement_valid, axis=1)].reset_index(drop=True)
+
+    def get_by_id(self, measurement_id: int) -> pd.DataFrame:
         with self.session_factory() as session:
-            measurements = session.query(Measurement).filter(Measurement.id == measurement_id).first()
-            if not measurements:
-                return None
-            return self._map_to_dto(measurements)
+            query = session.query(Measurement).filter(Measurement.id == measurement_id)
+            df = pd.read_sql(query.statement, session.bind)
 
-    def get_all_by_sensor_id(self, sensor_id: int) -> list[MeasurementDTO]:
+            if df.empty:
+                return df
+
+            df["RecordedAt"] = self.convert_time_to_local(df["RecordedAt"])
+
+            return df[df.apply(self.is_measurement_valid, axis=1)].reset_index(drop=True)
+
+    def get_by_sensor_id(self, sensor_id: int) -> pd.DataFrame:
         with self.session_factory() as session:
-            # Alle Measurements mit der angegebenen SensorId abrufen
-            measurements = session.query(Measurement).filter(Measurement.sensor_id == sensor_id).all()
-            return [self._map_to_dto(m) for m in measurements]
+            query = session.query(Measurement).filter(Measurement.sensor_id == sensor_id)
+            df = pd.read_sql(query.statement, session.bind)
 
-    def get_all_by_sensor_id_since(self, sensor_id: int, since: datetime) -> list[MeasurementDTO]:
+            if df.empty:
+                return df
+
+            df["RecordedAt"] = self.convert_time_to_local(df["RecordedAt"])
+
+            return df[df.apply(self.is_measurement_valid, axis=1)].reset_index(drop=True)
+
+    def get_by_sensor_id_since(self, sensor_id: int, since: datetime) -> pd.DataFrame:
         with self.session_factory() as session:
-            measurements = session.query(Measurement).filter(Measurement.sensor_id == sensor_id,Measurement.recorded_at>since).all()
-            return [self._map_to_dto(m) for m in measurements]
+            query = session.query(Measurement).filter(Measurement.sensor_id == sensor_id, Measurement.recorded_at > since)
+            df = pd.read_sql(query.statement, session.bind)
 
-    # Messung hinzufügen
-    def add(self, dto: MeasurementDTO) -> MeasurementDTO:
-        from datetime import datetime
-        from DataAcces.Models.Measurement import Measurement
+            if df.empty:
+                return df
 
-        with self.session_factory() as session:
-            entity = Measurement(
-                value=dto.value,
-                unit=dto.unit,
-                type=dto.type,
-                sensor_id=dto.sensor_id,
-                sensor_id_reference=dto.sensor_id_reference,
-                recorded_at=dto.recorded_at or datetime.now(timezone.utc)
-            )
-            session.add(entity)
-            session.commit()
-            session.refresh(entity)
-            return self._map_to_dto(entity)
+            df["RecordedAt"] = self.convert_time_to_local(df["RecordedAt"])
 
-    # Hilfsmethode: Entity → DTO
-    def _map_to_dto(self, measurement: Measurement) -> MeasurementDTO:
-        station_dto = None
-        sensor_dto = None
-        if measurement.sensor.station:
-            station = measurement.sensor.station
-            station_dto = StationDTO(
-                id=station.id,
-                mac_address= station.mac_address,
-                location= station.location,
-                sensor_count= station.sensor_count,
-                created_at= station.created_at
+            return df[df.apply(self.is_measurement_valid, axis=1)].reset_index(drop=True)
 
-            )
-        if measurement.sensor:
-            sensor = measurement.sensor
-            sensor_dto = SensorDTO(
-                id=sensor.id,
-                type=sensor.type,
-                unit=sensor.unit,
-                device_id=sensor.device_id,
-                station_id=sensor.station_id,
-                station=station_dto,
-                created_at=sensor.created_at,
-                updated_at=sensor.updated_at
-            )
 
-        return MeasurementDTO(
-            id=measurement.id,
-            value=measurement.value,
-            unit=measurement.unit,
-            type=measurement.type,
-            sensor_id=measurement.sensor_id,
-            sensor_id_reference=measurement.sensor_id_reference,
-            sensor=sensor_dto,
-            recorded_at=measurement.recorded_at,
-            created_at=measurement.created_at,
-            updated_at=datetime.now(timezone.utc)
-        )
+    @staticmethod
+    def convert_time_to_local(series: pd.Series):
+        series = pd.to_datetime(series)
+
+        if series.dt.tz is None:
+            series = series.dt.tz_localize('UTC')
+        else:
+            series = series.dt.tz_convert('UTC')
+
+        local_tz = datetime.now().astimezone().tzinfo
+        series = series.dt.tz_convert(local_tz)
+
+        series = series.dt.tz_localize(None)
+
+        return series
+
+    def is_measurement_valid(self, row) -> bool:
+        value = row['Value']
+        measurement_type = row['Type']
+
+        if not ((isinstance(value, float) or isinstance(value, int)) and isinstance(measurement_type, str)):
+            return False
+
+        if measurement_type == 'temperature' and value<=60 and value >= -40:
+            return True
+        elif measurement_type == 'humidity' and value<=100 and value >= 0:
+            return True
+        elif measurement_type == 'soil_moisture' and value<=100 and value >= 0:
+            return True
+        else:
+            return False
