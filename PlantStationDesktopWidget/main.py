f985e3814a8b47d3b01c5e5ca39e64f47c32f393
max <maxwoehlke@web.de>
2026-05-25 15:03:46 +0200

    refact: relative pathfinding for css-stylesheets

--- PlantStationDesktopWidget/main.py
+++ PlantStationDesktopWidget/main.py
@@ -4,6 +4,14 @@ from DataAcces.Repositories.StationRepo import StationRepo
 from DataAcces.Repositories.SensorRepo import SensorRepo
 from MainWindow import MainWindow
 from ViewModels.MainWindowViewModel import MainWindowViewModel
+from pathlib import Path
+
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return Path(sys._MEIPASS) / relative_path
+    return Path(__file__).parent / relative_path
+
 
 def main():
     app = QApplication(sys.argv)
@@ -12,7 +20,7 @@ def main():
     sensor_repo = SensorRepo()
     main_window_view_model = MainWindowViewModel(station_repo=station_repo, sensor_repo=sensor_repo)
 
-    with open("StyleSheets/styles.css", "r") as file:
+    with open(resource_path("StyleSheets/styles.css"), "r") as file:
         app.setStyleSheet(file.read())
 
     window = MainWindow(view_model=main_window_view_model)
