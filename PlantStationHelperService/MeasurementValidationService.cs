03ab35becfee73af5b812a07e4e17dba34f0d664
max <maxwoehlke@web.de>
2026-08-27 22:41:42 +0200

    feat: Adding MeasurementValidationService.cs to validate measurements values by type-column for the plot.

---
+++ PlantStationHelperService/MeasurementValidationService.cs
@@ -0,0 +1,11 @@
+using DataAccess.Models;
+namespace PlantStationHelperService;
+
+public class MeasurementValidationService
+{
+    public List<Measurement> ValidateMeasurements(List<Measurement> measurements)
+    {
+        
+        return measurements;
+    }
+}
